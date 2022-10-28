18eb260ec86211ce12126c5cf4382e6c1e36f65a
Justina Zymonaite <jzymonaite@yahoo.com>
2022-10-28 15:58:00 +0300

    flex praktinio taikymo pvz

--- flask_grid.py
+++ flask_grid.py
@@ -18,5 +18,13 @@ def grid():
 def fullpage():
     return render_template("styling/fullpage.html")
 
+@app.route("/flex")
+def flex():
+    return render_template("styling/flex.html")
+
+@app.route("/flexpage")
+def flexpage():
+    return render_template("styling/flexpage.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
